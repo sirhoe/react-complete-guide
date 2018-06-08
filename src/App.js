f9d42fc07f62819398d0c6574a0d5f5c97e67d53
sirhoe <sirhoe@gmail.com>
2018-06-08 12:06:07 +1000

    c30 understanding jsx. v2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() { 
+  render() {
     return (
       <div className="App">
-       <h1>Hi! I am a React App. </h1>
+        <h1>Hi! I am a React App. </h1>
       </div>
     );
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
